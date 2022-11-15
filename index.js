1c6a7f58f8db93a009a74f33730dda523424d9cb
VolxDarkTV <vinvenzo.foti@outlook.it>
2022-11-15 08:47:33 +0100

    fixed li

--- index.js
+++ index.js
@@ -3,15 +3,35 @@ let lista = ['prosciutto', 'sedano', 'banane', 'salame']
 // Collego le variabili al ID
 let data = document.getElementById('data');
 // let stampa = document.getElementById('print').innerHTML += '<li class=elementi></li>';
-// let send = document.getElementById('send');
+let send = document.getElementById('send');
+
+// PROVA CON FOR
+
+// for(i = 0; i < lista.length; i++){
+//     let stampa = document.getElementById('print').innerHTML += '<li class=elementi></li>';
+
+//     let stampaElementi = document.getElementsByClassName('elementi');
+//     stampaElementi.innerHTML += lista[i -1];
+
+//     console.log(lista[i -1]);
+// }
 
 let i = 0;
+
 while(i < lista.length){
     // let requestName = prompt('yours name');
     // console.log(requestName);
-    let stampa = document.getElementById('print').innerHTML += '<li class=elementi></li>';
+    let stampa = document.getElementById('print').innerHTML += `<li class=elementi>${lista[i]}</li>`;
 
-    let stampaElementi = document.getElementsByClassName('elementi').innerHTML += lista;
+    // let stampaElementi = document.getElementsByClassName('elementi');
+    // stampaElementi.innerHTML += lista[i -1];
     i++
-    console.log(lista[i]);
+    console.log(lista[i -1]);
 }
+
+// Configuro il bottone
+// send.addEventListener('click',
+//     function(){
+
+//     }
+// );
